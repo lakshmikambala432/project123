ed89bb71bea1477a78cca791391dda6037c30341
HP <HP@devi>
2023-10-09 20:01:49 +0530

    first commit

---
+++ encapsulation/src/com/tnsif/encapsulation/Encapsulation.java
@@ -0,0 +1,15 @@
+package com.tnsif.encapsulation;
+
+public class Encapsulation {
+	public int serialno;
+	public int String name;
+	public int age;
+	
+	//member function
+	
+	public void show () {
+		System.out.println("serialno"+ serislno+ "\t"+ "name"+ name+ "\t"+ "age"+age);
+	}
+
+
+}
