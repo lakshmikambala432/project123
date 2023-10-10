72e04da048d2641dd38b294f4d75019da295cbc2
HP <HP@devi>
2023-10-10 16:01:07 +0530

    first commit

---
+++ singleinteritance_/src/com/tnsif/singleinteritance_/SingleDemo.java
@@ -0,0 +1,12 @@
+package com.tnsif.singleinteritance_;
+
+public class SingleDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student st=new Student("Lakshmi",1837246104,"Chennai",9898989);
+		System.out.println(st);
+
+	}
+
+}
