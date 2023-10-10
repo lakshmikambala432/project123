e20c1784e5ab8c09d294310c0b0a7eebd6614dfd
HP <HP@devi>
2023-10-10 16:13:21 +0530

    first commit

---
+++ singleinherit/src/com/tnsif/singleinherit/Demo.java
@@ -0,0 +1,22 @@
+package com.tnsif.singleinherit;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student s =new Student(1,"anu");
+		if(s instanceof Registerable)
+		{
+			System.out.println("Student Registered");
+		}
+		else
+		    {
+				System.out.println("Student Not Registered");
+			}
+
+		}
+		}
+
+	}
+
+}
